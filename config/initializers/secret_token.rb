c5ed80c6bca515fcb7b3a147475132e8800b596c
Julian <julian@JuliansMacBook.local>
2015-08-31 09:15:08 +1200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PlanetExpress::Application.config.secret_key_base = '3fbc009613a9cc1f9a1befc75e3d055926c4015677d2915793a05a9be88a0b6d05b3e0aaa87d5f985041de6879fc5f86021a96e1a86728852ea15dad1755a915'
